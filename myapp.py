478d8287a53fb3e0ecba250949c8b1eb36f1cd1c
jaypat3 <jaypatwardhan3@gmail.com>
2022-12-18 22:06:07 -0500

    loaded all datasets

--- myapp.py
+++ myapp.py
@@ -6,32 +6,34 @@ import matplotlib.pyplot as plt
 
 
 st.title('Methylation Portal')
-BLCA = './TCGADatasets/BLCA/GeneTableBLCAPVals.csv'
-BRCA = './TCGADatasets/BRCA/GeneTableBRCAPVals.csv'
-CESC = './TCGADatasets/CESC/GeneTableCESCPVals.csv'
-COAD = './TCGADatasets/COAD/GeneTableCOADPVals.csv'
-ESCA = './TCGADatasets/ESCA/GeneTableESCAPVals.csv'
-GBM = './TCGADatasets/GBM/GeneTableGBMPVals.csv'
-HNSC = './TCGADatasets/HNSC/GeneTableHNSCPVals.csv'
-KIRC = './TCGADatasets/KIRC/GeneTableKIRCPVals.csv'
-KIRP = './TCGADatasets/KIRP/GeneTableKIRPPVals.csv'
-LGG = './TCGADatasets/LGG/GeneTableLGGPVals.csv'
-LIHC = './TCGADatasets/LIHC/GeneTableLIHCPVals.csv'
-LUAD = './TCGADatasets/LUAD/GeneTableLUADPVals.csv'
-LUSC = './TCGADatasets/LUSC/GeneTableLUSCPVals.csv'
-OV = './TCGADatasets/OV/GeneTableOVPVals.csv'
-PAAD = './TCGADatasets/PAAD/GeneTablePAADPVals.csv'
-PCPG = './TCGADatasets/PCPG/GeneTablePCPGPVals.csv'
-PRAD = './TCGADatasets/PRAD/GeneTablePRADPVals.csv'
-READ = './TCGADatasets/READ/GeneTableREADPVals.csv'
-SARC = './TCGADatasets/SARC/GeneTableSARCPVals.csv'
-SKCM = './TCGADatasets/SKCM/GeneTableSKCMPVals.csv'
-STAD = './TCGADatasets/STAD/GeneTableSTADPVals.csv'
-TGCT = './TCGADatasets/TGCT/GeneTableTGCTPVals.csv'
-THCA = './TCGADatasets/THCA/GeneTableTHCAPVals.csv'
-THYM = './TCGADatasets/THYM/GeneTableTHYMPVals.csv'
-UCEC = './TCGADatasets/UCEC/GeneTableUCECPVals.csv'
+BLCAdata = './TCGADatasets/BLCA/GeneTableBLCAPVals.csv'
+BRCAdata = './TCGADatasets/BRCA/GeneTableBRCAPVals.csv'
+CESCdata = './TCGADatasets/CESC/GeneTableCESCPVals.csv'
+COADdata = './TCGADatasets/COAD/GeneTableCOADPVals.csv'
+ESCAdata = './TCGADatasets/ESCA/GeneTableESCAPVals.csv'
+GBMdata = './TCGADatasets/GBM/GeneTableGBMPVals.csv'
+HNSCdata = './TCGADatasets/HNSC/GeneTableHNSCPVals.csv'
+KIRCdata = './TCGADatasets/KIRC/GeneTableKIRCPVals.csv'
+KIRPdata = './TCGADatasets/KIRP/GeneTableKIRPPVals.csv'
+LGGdata = './TCGADatasets/LGG/GeneTableLGGPVals.csv'
+LIHCdata = './TCGADatasets/LIHC/GeneTableLIHCPVals.csv'
+LUADdata = './TCGADatasets/LUAD/GeneTableLUADPVals.csv'
+LUSCdata = './TCGADatasets/LUSC/GeneTableLUSCPVals.csv'
+OVdata = './TCGADatasets/OV/GeneTableOVPVals.csv'
+PAADdata = './TCGADatasets/PAAD/GeneTablePAADPVals.csv'
+PCPGdata = './TCGADatasets/PCPG/GeneTablePCPGPVals.csv'
+PRADdata = './TCGADatasets/PRAD/GeneTablePRADPVals.csv'
+READdata = './TCGADatasets/READ/GeneTableREADPVals.csv'
+SARCdata = './TCGADatasets/SARC/GeneTableSARCPVals.csv'
+SKCMdata = './TCGADatasets/SKCM/GeneTableSKCMPVals.csv'
+STADdata = './TCGADatasets/STAD/GeneTableSTADPVals.csv'
+TGCTdata = './TCGADatasets/TGCT/GeneTableTGCTPVals.csv'
+THCAdata = './TCGADatasets/THCA/GeneTableTHCAPVals.csv'
+THYMdata = './TCGADatasets/THYM/GeneTableTHYMPVals.csv'
+UCECdata = './TCGADatasets/UCEC/GeneTableUCECPVals.csv'
 
+all_data = [BLCAdata,BRCAdata,CESCdata,COADdata,ESCAdata,GBMdata,HNSCdata,KIRCdata,KIRPdata,LGGdata,LIHCdata,LUADdata,LUSCdata,OVdata,PAADdata,PCPGdata,PRADdata,READdata,SARCdata,SKCMdata,STADdata,TGCTdata,THCAdata,THYMdata,UCECdata]
+BLCA,BRCA,CESC,COAD,ESCA,GBM,HNSC,KIRC,KIRP,LGG,LIHC,LUAD,LUSC,OV,PAAD,PCPG,PRAD,READ,SARC,SKCM,STAD,TGCT,THCA,THYM,UCEC = None
 all_datasets = [BLCA,BRCA,CESC,COAD,ESCA,GBM,HNSC,KIRC,KIRP,LGG,LIHC,LUAD,LUSC,OV,PAAD,PCPG,PRAD,READ,SARC,SKCM,STAD,TGCT,THCA,THYM,UCEC]
 
 @st.cache
@@ -47,8 +49,8 @@ def load_gene_data(dataset):
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-for dataset in all_datasets:
-    dataset = load_data(dataset)
+for i,data in enumerate(all_data):
+    all_datasets[i] = load_data(data)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache)")
 
@@ -64,5 +66,5 @@ BLCA_gene_data = load_gene_data(BLCA)
 st.write(BLCA_gene_data)
 
 fig,ax = plt.subplots()
-ax.hist = plt.hist(BRCA_gene_data[['MedianBvalTumor']],bins = np.arange(0,1.2,0.2))
+ax.hist = plt.hist(BLCA_gene_data[['MedianBvalTumor']],bins = np.arange(0,1.2,0.2))
 st.pyplot(fig)
