d18364226d4e3b14c8a11f6985f65e3c3d5db57d
jaypat3 <jaypatwardhan3@gmail.com>
2022-12-18 21:47:39 -0500

    loaded all datasets

--- myapp.py
+++ myapp.py
@@ -6,12 +6,37 @@ import matplotlib.pyplot as plt
 
 
 st.title('Methylation Portal')
-#hi
-BRCA = './TCGADatasets/BLCA/GeneTableBLCAPVals.csv'
+BLCA = './TCGADatasets/BLCA/GeneTableBLCAPVals.csv'
+BRCA = './TCGADatasets/BRCA/GeneTableBRCAPVals.csv'
+CESC = './TCGADatasets/CESC/GeneTableCESCPVals.csv'
+COAD = './TCGADatasets/COAD/GeneTableCOADPVals.csv'
+ESCA = './TCGADatasets/ESCA/GeneTableESCAPVals.csv'
+GBM = './TCGADatasets/GBM/GeneTableGBMPVals.csv'
+HNSC = './TCGADatasets/HNSC/GeneTableHNSCPVals.csv'
+KIRC = './TCGADatasets/KIRC/GeneTableKIRCPVals.csv'
+KIRP = './TCGADatasets/KIRP/GeneTableKIRPPVals.csv'
+LGG = './TCGADatasets/LGG/GeneTableLGGPVals.csv'
+LIHC = './TCGADatasets/LIHC/GeneTableLIHCPVals.csv'
+LUAD = './TCGADatasets/LUAD/GeneTableLUADPVals.csv'
+LUSC = './TCGADatasets/LUSC/GeneTableLUSCPVals.csv'
+OV = './TCGADatasets/OV/GeneTableOVPVals.csv'
+PAAD = './TCGADatasets/PAAD/GeneTablePAADPVals.csv'
+PCPG = './TCGADatasets/PCPG/GeneTablePCPGPVals.csv'
+PRAD = './TCGADatasets/PRAD/GeneTablePRADPVals.csv'
+READ = './TCGADatasets/READ/GeneTableREADPVals.csv'
+SARC = './TCGADatasets/SARC/GeneTableSARCPVals.csv'
+SKCM = './TCGADatasets/SKCM/GeneTableSKCMPVals.csv'
+STAD = './TCGADatasets/STAD/GeneTableSTADPVals.csv'
+TCGT = './TCGADatasets/TCGT/GeneTableTCGTPVals.csv'
+THCA = './TCGADatasets/THCA/GeneTableTHCAPVals.csv'
+THYM = './TCGADatasets/THYM/GeneTableTHYMPVals.csv'
+UCEC = './TCGADatasets/UCEC/GeneTableUCECPVals.csv'
+
+all_datasets = [BLCA,BRCA,CESC,COAD,ESCA,GBM,HNSC,KIRC,KIRP,LGG,LIHC,LUAD,LUSC,OV,PAAD,PCPG,PRAD,READ,SARC,SKCM,STAD,TCGT,THCA,THYM,UCEC]
 
 @st.cache
-def load_data():
-    data = pd.read_csv(BRCA)
+def load_data(dataset):
+    data = pd.read_csv(dataset)
     return data
 
 @st.cache
@@ -22,7 +47,8 @@ def load_gene_data(dataset):
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-BRCA = load_data()
+for dataset in all_datasets:
+    dataset = load_data(dataset)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache)")
 
@@ -32,10 +58,10 @@ if text_input:
     st.write("You entered: ", text_input)
 
 st.subheader('Raw data')
-st.write(BRCA)
+st.write(BLCA)
 
-BRCA_gene_data = load_gene_data(BRCA)
-st.write(BRCA_gene_data)
+BLCA_gene_data = load_gene_data(BLCA)
+st.write(BLCA_gene_data)
 
 fig,ax = plt.subplots()
 ax.hist = plt.hist(BRCA_gene_data[['MedianBvalTumor']],bins = np.arange(0,1.2,0.2))
